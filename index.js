83513ffe13434a6b462632146f3d14151621bb31
aldoso <alexdobras@gmail.com>
2017-10-06 12:07:22 +0300

    add files

---
+++ index.js
@@ -0,0 +1,46 @@
+//animation code    
+
+window.sr = ScrollReveal();
+
+//sr.reveal('nav', {
+//    duration: 1000,
+//    origin: 'top',
+//    delay: 500
+//
+//});
+//
+sr.reveal('#subtitlu h1', {
+    duration: 600,
+    origin: 'top',
+    distance: '50px',
+    delay: 50
+});
+
+sr.reveal('#subtitlu h4', {
+    duration: 1500,
+    origin: 'bottom',
+    distance: '50px',
+    delay: 1000
+});
+
+sr.reveal('#subtitlu a', {
+    duration: 1000,
+    origin: 'bottom',
+    distance: '200px',
+    delay: 500
+});
+
+sr.reveal('.stanga', {
+    duration: 800,
+    origin: 'left',
+    distance: '200px',
+    delay: 100
+});
+
+
+sr.reveal('.dreapta', {
+    duration: 800,
+    origin: 'left',
+    distance: '200px',
+    delay: 100
+});
